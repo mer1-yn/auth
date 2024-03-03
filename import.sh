72a708d473d8b3bd0e0056152ed3e359f1ded4fe
lunaris <41453367+lunaris@users.noreply.github.com>
2024-03-03 08:18:54 -0500

    should fully work, tested some other stuff and may add more flags later

--- import.sh
+++ import.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
+authdir=$(realpath $(dirname $0))
+cd $authdir
 f_flag=false
 g_flag=false
 a_flag=false
 e_flag=true
-fval="~/.ssh/generated"
+fval="/home/$whoami/.ssh/generated"
 
-while getopts 'af::eg' flag; do
+print_usage() { printf '\nUsage: import.sh: \n\t [-a] Add local keys \n\t [-f FILENAME] Write to file. Defaults to ~/.ssh/generated \n\t [-g] Generate new keylist\n'; exit 2; }
+
+while getopts 'af:eg' flag; do
   case "${flag}" in
     a) a_flag='true' ;;
     f) f_flag='true' 
@@ -14,12 +17,13 @@ while getopts 'af::eg' flag; do
     e) e_flag='false' 
     	f_flag='true';;
     g) g_flag='true' ;;
-    *) printf '\nUsage: %s: \n\t [-a] Add local keys \n\t [-f FILENAME] Write to file \n\t [-g] Generate new keylist\n' $0; exit 2 ;;
+    *) print_usage ;;
   esac
 done
 
 if [ $a_flag == false ] && [ $g_flag == false ] && [ $f_flag == false ]; then 
-	printf '%s: no arguments: \n\nUsage: %s: \n\t [-a] Add local keys \n\t [-f FILENAME] Write to file \n\t [-g] Generate new keylist\n' $0; 
+	printf '%s: no arguments:'$0 
+	print_usage
 	exit 2
 fi
 moi="$(git config user.name)"
@@ -31,15 +35,14 @@ if $a_flag; then
 		echo $file
 		grep -qxF "$(cat $file)" $moi || cat $file >> $moi
 	done
-	grep -qxF $moi keyfiles || echo $moi >> keyfiles
+	grep -qxF $moi $authdir/keyfiles || echo $moi >> $authdir/keyfiles
 fi
 
 if $g_flag; then
 	test -f generated && rm generated
 	echo ---SSH Keyfile generated $(date)--- >> generated
-	for file in $(cat keyfiles)
-	do	
-		echo $file
+	for file in $(cat keyfiles | grep -v $moi)
+	do
 		cat $file >> generated
 		echo >> generated
 	done
@@ -47,8 +50,8 @@ fi
 
 if $f_flag; then
 	if $e_flag; then
-		if [ !$(grep "AuthorizedKey.*generated" /etc/ssh/sshd_config) ] ; then
-			echo "Your sshd_config file is not configured to look at the $(dirname $0)/generated file. Please modify this in /etc/ssh/sshd_config"
+		if [ !$(grep "AuthorizedKey.*$fval" /etc/ssh/sshd_config) ] ; then
+			echo "Your sshd_config file is not configured to look at the $(realpath $fval) file. Please modify this in /etc/ssh/sshd_config"
 			exit 1
 		fi
 	fi 
