423f087d4003a2fc02e81d252f0da747d51bedd2
Lupin <41453367+lupin@users.noreply.github.com>
2024-03-03 03:13:41 -0500

    need to add a second computer lol

---
+++ import.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+moi="$(git config user.name)"
+
+for file in $(ls ~/.ssh/*.pub)
+do
+	echo $file
+
+	grep -qxF "$(cat $file)" $moi || echo $(cat $file) >> $moi
+done
+
+for file in $(ls . | grep -vE "(import.sh|$moi|generated)" )
+do 
+	cat file >> generated
+done
